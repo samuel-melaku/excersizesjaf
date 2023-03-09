87168c5b738a8b074f0656b28c2c467014b4ae3a
samuel-melaku <107581205+samuel-melaku@users.noreply.github.com>
2023-03-08 18:22:29 -0800

    waefawef
    aefwfew

--- excercise1.py
+++ excercise1.py
@@ -3,7 +3,7 @@ num2 = 30
 # expected output is 700
 
 def productCheck(num1, num2):
-    if num1 * num2 <= 1000:
+    if num1 * num2 <= 1000: # a check to see if the product of the 2 numbers is over 100
         return num1 * num2
-    else:
+    else: # the contrary 
         return num1 + num2
